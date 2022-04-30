9653ac44a97cc4d4b2e075a1f94477cc1211f1e1
Weronika Dąbrowicz <223073@edu.p.lodz.pl>
2022-04-30 19:27:57 +0200

    3.1. Zmiana nazewnictwa klas

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 int totalRejected = 0;
 
@@ -15,6 +15,6 @@ int totalRejected = 0;
 		super.countIn(in);
 	}
 }
-//edytowany plik defaultCountingOutRhymer, FIFORhymer, HanoiRhymer
+//edytowany plik DefaultCountingOutRhymer, FIFORhymer, HanoiRhymer
 //plik sprawdzony pod zwględem błędów formatowania
 //komentarz dodany 30.04.2022r dąbrowicz weronika
